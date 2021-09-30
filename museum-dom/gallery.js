fb6992c2cf66990181ca02bd56d96c76cc22eaec
Inga Baranets <rubiaqute@gmail.com>
2021-09-30 12:35:15 +0400

    feat: add gallery animation

--- museum-dom/gallery.js
+++ museum-dom/gallery.js
@@ -28,7 +28,44 @@ img[i]=document.createElement('img');
 img[i].classList.add('gallery-img');
 img[i].src=srcArray[i];
 img[i].alt='';
+img[i].classList.add('_anim-items');
 pictureInnerContainer.append(img[i]);
 }
 }
 GetIMG();
+
+const animItems = document.querySelectorAll('._anim-items');
+if (animItems.length >0){
+    window.addEventListener('scroll', animOnScroll)
+    document.addEventListener("DOMContentLoaded",animOnScroll)
+    
+    function animOnScroll () {
+        
+        for(let i=0; i<animItems.length; i++){
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 5;
+
+            let animItemPoint = window.innerHeight -animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            
+            if (scrollY >animItemOffset - animItemPoint){
+                animItem.classList.add('_active');
+            } else {
+                animItem.classList.remove('_active')
+            }
+
+        }
+    
+    }
+    function offset(el){
+        const rect =el.getBoundingClientRect();
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top +scrollTop, left:rect.left +scrollLeft}
+    }
+    
+}
