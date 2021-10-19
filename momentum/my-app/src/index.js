5fc28fa64e19c47c6cc55137d87d3127c9272d10
rubiaqute <rubiaqute@gmail.com>
2021-10-20 02:02:12 +0400

    feat: add weather vidjet

--- momentum/my-app/src/index.js
+++ momentum/my-app/src/index.js
@@ -6,6 +6,14 @@ const body =document.querySelector('body');
 let randomNumber =getRandomNum();
 const slideNext = document.querySelector('.slide-next');
 const slidePrev = document.querySelector('.slide-prev');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const windSpeed = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity')
+const weatherDescription = document.querySelector('.weather-description');
+const weatherError = document.querySelector('.weather-error');
+const cityUser = document.querySelector('.city');
+
 
 
 
@@ -21,9 +29,10 @@ function showTime() {
 
 function showDate(){
     const date = new Date();
-    const options = {weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC'};
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
     const currentDate = date.toLocaleDateString('en-US', options);
     dateApp.textContent = currentDate;
+    
 }
 
 function showGreeting(){
@@ -49,6 +58,7 @@ function getTimeOfDay()
 
 function setLocalStorage() {
     localStorage.setItem('name', nameUser.value);
+    localStorage.setItem('city', cityUser.value);
   }
   window.addEventListener('beforeunload', setLocalStorage)
 
@@ -56,6 +66,13 @@ function setLocalStorage() {
     if(localStorage.getItem('name')) {
       nameUser.value = localStorage.getItem('name');
     }
+    if(localStorage.getItem('city')) {
+        cityUser.value = localStorage.getItem('city');
+        getWeather();
+      } else{
+           cityUser.value = "Минск";
+           getWeather();
+      }
   }
   window.addEventListener('load', getLocalStorage);
 
@@ -84,7 +101,32 @@ function getSlideNext(){
     setBg();
  }
 
+ async function getWeather() {  
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityUser.value}&lang=en&appid=e57257892292e552c0054a48a736b278&units=metric`;
+        const res = await fetch(url);
+        const data = await res.json(); 
+        weatherError.textContent = '';
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${Math.round(data.main.temp)}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+        windSpeed.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+        humidity.textContent = `Humidity: ${Math.round(data.main.humidity)}%`;
+      } catch(err) {
+        weatherError.textContent = `Error! city not found for '${cityUser.value}'!`;
+        weatherIcon.className = 'weather-icon owf';
+        temperature.textContent = ``;
+        weatherDescription.textContent = '';
+        windSpeed.textContent = ``;
+        humidity.textContent = '';
+      }
+  }
+  
+
 setBg();
 showTime();
+// getWeather()
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
+cityUser.addEventListener('change', getWeather);
