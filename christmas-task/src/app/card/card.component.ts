6c31882bb434ec224830f5190a582c1ec778a9d6
rubiaqute <rubiaqute@gmail.com>
2021-12-09 02:07:26 +0300

    init: start project christmass-task

---
+++ christmas-task/src/app/card/card.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.componenent.html',
+  styleUrls: ['./card.component.scss']
+})
+
+export class CardComponent {}
