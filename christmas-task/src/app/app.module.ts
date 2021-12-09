6e1a8146db75b69de3ecb818110a575241f34ad6
rubiaqute <rubiaqute@gmail.com>
2021-12-09 17:04:40 +0300

    feat: add initial structure and scss base files

--- christmas-task/src/app/app.module.ts
+++ christmas-task/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru'
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
+
+
+registerLocaleData(localeRu, 'ru')
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // RouterModule.forRoot([
+    //   { path: '', component: ProductListComponent },
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- christmas-task/src/app/card/card.component.ts
+++ christmas-task/src/app/card/card.component.ts
@@ -1,9 +1,45 @@
-import {Component} from '@angular/core';
+// import {Component, OnInit, Input} from '@angular/core';
+// // import { Card } from '../app.component';
 
-@Component({
-  selector: 'app-card',
-  templateUrl: './card.componenent.html',
-  styleUrls: ['./card.component.scss']
-})
 
-export class CardComponent {}
+// @Component({
+//   selector: 'app-card',
+//   templateUrl: './card.componenent.html',
+//   styleUrls: ['./card.component.scss'],
+// })
+
+// export class CardComponent implements OnInit {
+//   @Input() card: Card = {
+//     title:'',
+//     text:''
+//   }
+//   @Input() index: number | undefined;
+//   cardDate: Date = new Date();
+  
+//   // title: string = 'My Card Title'
+//   // text: string = 'My simple text'
+//   number = 42
+//   imgUrl: string = 'https://img1.pnghut.com/0/12/2/J7tA0RX6PL/logo-directive-data-binding-web-application-brand.jpg'
+//   disabled:boolean = false;
+//   textColor:string = 'black'
+//   getInfo(): string {
+//     return 'This is my info'
+//   }
+//   changeTitle() {
+//     this.card.title = "This title has been changed"
+//   }
+//   inputHandler(value: any) {
+//     this.card.title = value
+//   }
+//   changeHandler() {
+//     console.log(this.card.title)
+//   }
+//   ngOnInit(): void {
+//     setTimeout ( () => {
+//       this.imgUrl = 'https://pluspng.com/img-png/angular-logo-png-javascript-logo-png-img-512-512-free-transparent-900x520.jpg'
+//       this.disabled = true;
+    
+//     }, 3000)
+    
+//   }
+// }
