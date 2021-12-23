fe926a3a466a4dcb8f286ab35919f54b9204106e
rubiaqute <rubiaqute@gmail.com>
2021-12-23 14:29:53 +0300

    feat: duplicate toys and make counter change

--- christmas-task/src/app_views/toys-to-hang/toys-to-hang.component.ts
+++ christmas-task/src/app_views/toys-to-hang/toys-to-hang.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ElementRef, ViewChild, Input, ChangeDetectorRef, ChangeDetectionStrategy, AfterContentChecked } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild, TemplateRef, Input, ChangeDetectorRef, ChangeDetectionStrategy, AfterContentChecked } from '@angular/core';
 import { PositionServiceComponent } from 'src/app_services/position-service/position-service.component';
 import { ToyCard } from './../../app_models/interfaces';
 import { DecorateServiceComponent } from './../../app_services/decorate-service/decorate-service.component';
@@ -12,15 +12,42 @@ import { DecorateServiceComponent } from './../../app_services/decorate-service/
 export class ToysToHangComponent implements OnInit {
 
   // @Input() treeContainer!: ElementRef<HTMLImageElement>
+  toysArray: (string | number)[][][]=[];
   toysToHang: ToyCard[] = [];
   dragActive: boolean = false;
   toyDrag: HTMLElement= document.getElementById('toysContainer')!
   currentDroppable:HTMLElement|null=null;
-  handleDragStart(event:DragEvent,toy:ToyCard){
+  handleDragStart(event:DragEvent){
     this.positionService.rewriteIsAvailableToDrop(false);
-    const clone=(<HTMLElement>event.target)!.cloneNode(true);
+    // const draggedToy = (<HTMLElement>event.target)!
+    // const cloneToy=(<HTMLElement>event.target)!.cloneNode(true);
+    // (<HTMLElement>event.target)!.parentNode!.append(cloneToy)
     event.dataTransfer!.setData("dragToy", (<HTMLElement>event.target)!.id );
   }
+  ngOnInit(): void {
+    this.toysToHang = this.decorateService.getToysToHang();
+    this.toysArray=this.createToysArray();
+    console.log(this.toysArray);
+    console.log(this.toysToHang);
+  }
+  getCount(id: number): string {
+    const imageContainers = document.querySelectorAll('.tree-container__toy-image');
+    return String(imageContainers[id].childElementCount).padStart(2, '0');
+    
+  }
+  createToysArray(){
+    let toysArray:(string | number)[][][] = [];
+    this.toysToHang.forEach((el)=>{
+      let toy:(string | number)[][]=[];
+      for (let i=0; i<Number(el.count); i++){
+        toy.push([el.num, i])
+      }
+      toysArray.push(toy)
+    })
+return toysArray;
+  }
+
+  
   
   // @HostListener('window:mouseup', ['$event'])
   // mouseUp(event:MouseEvent){
@@ -61,14 +88,8 @@ export class ToysToHangComponent implements OnInit {
   // }
   constructor(private decorateService: DecorateServiceComponent, private cdr:ChangeDetectorRef, private positionService: PositionServiceComponent) {}
 
-  ngOnInit(): void {
-    this.toysToHang = this.decorateService.getToysToHang();
-    
-    console.log(this.toysToHang);
-  }
-  getCount(count: string): string {
-    return count.padStart(2, '0');
-  }
+  
+  
   drag(event: DragEvent, toyId:string) {
     event.preventDefault();
     this.toyDrag = event.target as HTMLElement;
